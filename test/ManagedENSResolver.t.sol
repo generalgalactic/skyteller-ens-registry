021262138aa8edefdef8bcf4dac16c6ec84674c4
Andrey Petrov <andrey.petrov@shazow.net>
2023-05-08 15:10:06 -0400

    test: Pull out helpers

--- test/ManagedENSResolver.t.sol
+++ test/ManagedENSResolver.t.sol
@@ -7,27 +7,7 @@ import "../src/ManagedENSResolver.sol";
 import "../src/ManagedRegistrar.sol";
 import "../src/IRegistrar.sol";
 
-library Helpers {
-
-    // Basic helper for generating a single-level namehash (does not recurse)
-    function namehash(bytes memory _tld) internal pure returns (bytes32) {
-        bytes32 base = bytes32(0x0);
-        bytes32 node = keccak256(_tld);
-        return keccak256(abi.encodePacked(base, node));
-    }
-
-    function namehash(bytes memory _domain, bytes memory _tld) internal pure returns (bytes32) {
-        bytes32 base = namehash(_tld);
-        bytes32 node = keccak256(_domain);
-        return keccak256(abi.encodePacked(base, node));
-    }
-
-    function namehash(bytes memory _subdomain, bytes memory _domain, bytes memory _tld) internal pure returns (bytes32) {
-        bytes32 base = namehash(_domain, _tld);
-        bytes32 node = keccak256(_subdomain);
-        return keccak256(abi.encodePacked(base, node));
-    }
-}
+import "./Helpers.sol";
 
 contract ManagedENSResolverTest is Test {
     ManagedENSResolver public resolver;
