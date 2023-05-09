656a72cfebcdb5e812f126d36b83f9e6417533d0
Andrey Petrov <andrey.petrov@shazow.net>
2023-05-09 16:49:44 -0400

    script/override.sh: Add example call for setSubnodeRecord

--- script/override.sh
+++ script/override.sh
@@ -17,6 +17,14 @@ cast rpc anvil_impersonateAccount "$OWNER"
 
 cast send --from "$OWNER" "$ENS_CONTRACT" "setResolver(bytes32, address)" "$NAMEHASH" "$NEW_RESOLVER"
 
+# One way to add a subdomain using the global registrar (74,392 gas)
+cast send --from "$OWNER" "$ENS_CONTRACT" "setSubnodeRecord(bytes32 node, bytes32 label, address owner, address resolver, uint64 ttl)" \
+    "$(cast namehash "skyteller.eth")" \
+    "$(cast keccak "owner")" \
+    "$OWNER" \
+    "$NEW_RESOLVER" \
+    "0"
+
 #cast rpc anvil_mine 1  # Should not be necessary?
 
 GOT_RESOLVER=$(cast call "$ENS_CONTRACT" "resolver(bytes32) returns (address)" "$NAMEHASH")
