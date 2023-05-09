52034ddd7b83a385f9892bb09309be620772926f
Andrey Petrov <andrey.petrov@shazow.net>
2023-05-09 16:50:52 -0400

    src/ManagedENSResolver.sol: Added resolver and resolve interfaces, works on ethers.js now

--- src/IResolver.sol
+++ src/IResolver.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.13;
 
 interface IResolver {
     function addr(bytes32 node) external view returns (address);
+    function resolver(bytes32 node) external view returns (address);
 }
--- src/ManagedENSResolver.sol
+++ src/ManagedENSResolver.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import {ExtendedResolver} from "ens-contracts/resolvers/profiles/ExtendedResolver.sol";
+
 import {IRegistrar} from "./IRegistrar.sol";
 import {IResolver} from "./IResolver.sol";
 
@@ -12,7 +14,7 @@ import {IResolver} from "./IResolver.sol";
 /// maintains the stored state of subdomain node-to-address mappings.
 /// @dev ManagedENSResolver can be replaced to add functionality without losing
 /// mapping state that is stored in the ManagedRegistrar.
-contract ManagedENSResolver is IResolver {
+contract ManagedENSResolver is IResolver, ExtendedResolver {
     // FIXME: Do we want this resolver to support other functions for the parent node?
 
     /// @notice IRegistrar manages the assignment mappings.
@@ -26,12 +28,18 @@ contract ManagedENSResolver is IResolver {
     function supportsInterface(bytes4 interfaceID) public pure returns (bool) {
         return interfaceID == 0x3b3b57de || // addr(bytes32 node) returns (address)
                // TODO: interfaceID == 0x691f3431 || // name(bytes32 node) returns (string memory);
-               // TODO: interfaceID == 0x9061b923 || // resolve(bytes calldata name, bytes calldata data) returns(bytes);
+               interfaceID == 0x9061b923 || // resolve(bytes calldata name, bytes calldata data) returns(bytes);
+               interfaceID == 0x0178b8bf || // resolver(bytes32 node) returns (address)
                interfaceID == 0x01ffc9a7;   // supportsInterface
     }
 
     function addr(bytes32 nodeID) external view returns (address) {
         return registrar.addr(nodeID);
+    }
+
+    function resolver(bytes32) external view returns (address) {
+        // This resolver handles all subnodes.
+        return address(this);
     }
 
     // Per Resolver specification: https://eips.ethereum.org/EIPS/eip-137#resolver-specification
